b2fef7eaa32696cab81cbc006c7776fa7b3ece5a
Pavani <110219104+PAVANIJANJIKA@users.noreply.github.com>
2022-08-26 22:16:54 +0530

    Add files via upload

---
+++ prime or not using python.py
@@ -0,0 +1,12 @@
+#PROGRAM TO CHECK WHETHER GIVEN NUMBER IS PRIME OR NOT
+num=int(input("Enter a number:"))
+if num>1:
+    for i in range(2,num):
+        if(num%i)==0:
+            print(num,"is not a prime number")
+            break
+        else:
+          print(num,"is a prime number")
+          break
+else:
+    print(num,"is not a prime number")
